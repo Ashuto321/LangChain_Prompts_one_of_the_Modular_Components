52c140ed08955d3240de0f8429748e80fb7aa643
Ashutosh Pandey <jackworth970@gamil.com>
2026-05-24 13:01:41 +0530

    prompt files

--- prompts_interface.py
+++ prompts_interface.py
@@ -1,8 +1,7 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
-
+from langchain_core.prompts import PromptTemplate, load_prompt
 load_dotenv()
 
 st.header("Research Assistant")
@@ -17,27 +16,7 @@ style_input = st.selectbox("Select the style of Explanation",["Beginner-friendly
 
 length_input = st.selectbox("Select Explanation Length", ["short(1-2 patagraph)","medium(3-4 paragraph)","Long(detailed explanation)"])
 
-# creating the prompt template for the dynamic prompt
-template = PromptTemplate(
-    template ="""
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-
-Explanation Style:
-- Explain it as if I am a {style_input}.
-
-Mathematical Details:
-- Include relevant mathematical equations if present in the paper.
-- Explain mathematical concepts using simple, intuitive code snippets where applicable.
-
-Analogies:
-- Use relatable analogies to simplify complex concepts.
-
-If certain information is not available in the paper, respond with:
-"Insufficient information available instead of guessing."
-
-Ensure the summary is clear, accurate, and aligned with the provided style and length.""",
-input_variables = ["paper_input", "style_input", "length_input"]
-)
+template = load_prompt("prompt_template.json")
 
 # filling the placeholders(by creating the dictinonary)
 prompt=template.invoke({
